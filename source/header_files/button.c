786638a24917b5ff8c076b9a351662e66379eaac
Brenda Oliveira <1brendabarbosa@gmail.com>
2024-05-06 23:16:56 -0300

    incluindo get simples

--- source/header_files/button.c
+++ source/header_files/button.c
@@ -12,24 +12,24 @@ void game_state(int *state_p, int *buttons_p){
         state = 2 -> Partida em pausa
         state = 3 -> Finaliza a partida
 
-        button = 0 -> KEY_0 Encerra a partida
-        button = 1 -> KEY_1 Seleciona modo 1 jogador
+        button = 1 -> KEY_0 Encerra a partida
+        button = 2 -> KEY_1 Seleciona modo 1 jogador
         button = 4 -> KEY_2 Seleciona modo 2 jogadores
         button = 8 -> KEY_3 Pause/Play na partida
     */
    switch (*state_p)
    {
     case 0:  //Menu Inicial
-        if(*buttons_p == 1) *state_p = 1;  //seleciona modo single player
-        else if(*buttons_p == 2) *state_p = 2; //seleciona modo dual player
+        if(*buttons_p == 2) *state_p = 1;  //seleciona modo single player
+        else if(*buttons_p == 4) *state_p = 2; //seleciona modo dual player
     
         break;
     case 1:
-        if(*buttons_p == 0) *state_p = 0;
+        if(*buttons_p == 1) *state_p = 0;
         else if(*buttons_p == 8) *state_p = 2;
         break;
     case 2:
-        if(*buttons_p == 0) *state_p = 0;
+        if(*buttons_p == 1) *state_p = 0;
         else if(*buttons_p == 8) *state_p = 2;
         break;  
     case 3:
@@ -38,5 +38,24 @@ void game_state(int *state_p, int *buttons_p){
         // else if(*buttons_p == 8) *state_p = 2;
         break; 
 
+    }
+}
+
+void get_button(int *buttons_p, int *b_pressed){
+    switch (*buttons_p)
+    {
+    case 1:
+        *b_pressed = 0; 
+        break;
+    case 2:
+        *b_pressed = 1; 
+        break;
+    case 4:
+        *b_pressed = 2; 
+        break;
+    case 8:
+        *b_pressed = 3; 
+        break;
+    
     }
 }
